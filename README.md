9dfe53d27c6e25711668c2bdcb95c50387bd9720
adil.karaoz <adil.karaoz@alaricsecurities.com>
2020-06-05 22:30:34 +0300

    add cpu miner opt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # xmrig_requirements
-Repository contains Ubuntu 20.04 image which build xmrig tool to connect mining pool
+Repository contains Ubuntu 20.04 image which build xmrig tool to connect XMR mining pool and download cpu miner to connect
+any mining pool
--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 
 USER_NAME=$1
 
-sudo apt-get update && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+sudo apt-get update && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev wget -y
 
+# Download and build xmrig mining tool
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..
@@ -13,6 +14,16 @@ sudo ln -s /home/$USER_NAME/xmrig/build/xmrig /usr/local/bin/xmrig
 
 cat /sys/kernel/mm/transparent_hugepage/enabled
 sudo sysctl -w vm.nr_hugepages=8
+
+cd ../..
+
+# Download cpuminer-opt mining tool
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
+mkdir Cpuminer
+tar -xf Cpuminer-opt-cpu-pool-linux64.tar.gz -C Cpuminer
+rm -rf Cpuminer-opt-cpu-pool-linux64.tar.gz
+
+sudo ln -s /home/$USER_NAME/Cpuminer/cpuminer /usr/local/bin/cpuminer
 
 echo y | sudo ufw enable
 sudo ufw allow "OpenSSH" && sudo ufw allow 33824
